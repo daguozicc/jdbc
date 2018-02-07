7311b6eb03f01a91b828abe2237e28f293ce72de
大果子Cc <yzcxiaozhi@126.com>
2018-02-07 20:43:44 +0800

    新增删除查

--- src/com/mall/action/GoddessAction.java
+++ src/com/mall/action/GoddessAction.java
@@ -1,7 +1,10 @@
 package com.mall.action;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.mall.dao.GoddessDao;
 import com.mall.model.Goddess;
@@ -9,9 +12,25 @@ import com.mall.model.Goddess;
 public class GoddessAction {
 	public static void main(String[] args) throws Exception{
 		GoddessDao g = new GoddessDao();
+		
+//		List<Goddess> result = g.query("美洋洋","13","xi");
+		List<Map<String, Object>> params = new ArrayList<Map<String, Object>>();
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("name", "user_name");
+		param.put("rela", "like");
+		param.put("value", "'%美洋洋%'");
+		param = new HashMap<String, Object>();
+		param.put("name", "mobile");
+		param.put("rela", "=");
+		param.put("value", "'133'");
+		params.add(param);
+		List<Goddess> result = g.query(params);
+		for(int i=0;i<result.size();i++){
+			System.out.println(result.get(i).toString());
+		}
 		List<Goddess> gs=g.query();
 		for(Goddess goddess:gs){
-			System.out.println(goddess.getUser_name()+","+goddess.getAge());
+			//System.out.println(goddess.getUser_name()+","+goddess.getAge());
 		}
 		Goddess g1 = new Goddess();
 		g1.setUser_name("小果果");
@@ -27,6 +46,6 @@ public class GoddessAction {
 		//g.updateGoddess(g1);
 		//g.delGoddess(3);
 		Goddess g2 = g.get(2);
-		System.out.println(g2.toString());
+		//System.out.println(g2.toString());
 	}
 }
--- src/com/mall/dao/GoddessDao.java
+++ src/com/mall/dao/GoddessDao.java
@@ -8,11 +8,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.mall.db.DBUtil;
 import com.mall.model.Goddess;
 
 public class GoddessDao {
+	private String mobile;
+
 	public void addGoddess(Goddess g) throws Exception{
 		Connection  conn = DBUtil.getConnection();
 		String sql = ""+
@@ -80,7 +83,7 @@ public class GoddessDao {
 		Goddess g = null;
 		Connection  conn = DBUtil.getConnection();
 		String sql = ""+
-				" select * from imooc_goddess "+
+				" select * from  imooc_goddess "+
 				" where id=? ";
 		PreparedStatement ptmt = conn.prepareStatement(sql);
 		ptmt.setInt(1, id);
@@ -102,5 +105,72 @@ public class GoddessDao {
 			
 		}
 		return g;
+	}
+	
+	public List<Goddess>query(String name,String mobile,String email) throws Exception{
+		List<Goddess> result = new ArrayList<Goddess>();
+		Connection  conn = DBUtil.getConnection();
+		StringBuilder sb = new StringBuilder();
+		sb.append("select * from imooc_goddess");
+		sb.append(" where user_name like ? and mobile like ? and email like ? ");
+		PreparedStatement ptmt = conn.prepareStatement(sb.toString());
+		ptmt.setString(1, "%"+name+"%");
+		ptmt.setString(2, "%"+mobile+"%");
+		ptmt.setString(3, "%"+email+"%");
+		System.out.println(sb.toString());
+		ResultSet rs = ptmt.executeQuery();
+		Goddess g = null;
+		while(rs.next()){
+			g = new Goddess();
+			g.setId(rs.getInt("id"));
+			g.setUser_name(rs.getString("user_name"));
+			g.setAge(rs.getInt("age"));
+			g.setSex(rs.getInt("sex"));
+			g.setBirthday(rs.getDate("birthday"));
+			g.setEmail(rs.getString("email"));
+			g.setMobile(rs.getString("mobile"));
+			g.setCreate_date(rs.getDate("create_date"));
+			g.setCreate_user(rs.getString("create_user"));
+			g.setUpdate_date(rs.getDate("update_date"));
+			g.setUpdate_user(rs.getString("update_user"));
+			g.setIsdel(rs.getInt("isdel"));
+			
+			result.add(g);
+		}
+		return result;
+	}
+	public List<Goddess>query(List<Map<String, Object>> params) throws Exception{
+		List<Goddess> result = new ArrayList<Goddess>();
+		Connection  conn = DBUtil.getConnection();
+		StringBuilder sb = new StringBuilder();
+		sb.append("select * from imooc_goddess where 1=1 ");
+		if(params!=null&&params.size()>0){
+			for(int i=0;i<params.size();i++){
+				Map<String,Object> map=params.get(i);
+				sb.append(" and "+map.get("name")+" "+map.get("rela")+" "+map.get("value"));
+			}
+		}
+		PreparedStatement ptmt = conn.prepareStatement(sb.toString());
+		System.out.println(sb.toString());
+		ResultSet rs = ptmt.executeQuery();
+		Goddess g = null;
+		while(rs.next()){
+			g = new Goddess();
+			g.setId(rs.getInt("id"));
+			g.setUser_name(rs.getString("user_name"));
+			g.setAge(rs.getInt("age"));
+			g.setSex(rs.getInt("sex"));
+			g.setBirthday(rs.getDate("birthday"));
+			g.setEmail(rs.getString("email"));
+			g.setMobile(rs.getString("mobile"));
+			g.setCreate_date(rs.getDate("create_date"));
+			g.setCreate_user(rs.getString("create_user"));
+			g.setUpdate_date(rs.getDate("update_date"));
+			g.setUpdate_user(rs.getString("update_user"));
+			g.setIsdel(rs.getInt("isdel"));
+			
+			result.add(g);
+		}
+		return result;
 	}
 }
