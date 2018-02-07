fe51ea2380d5cc7d919573197c06d8cd3fb5ab79
大果子Cc <yzcxiaozhi@126.com>
2018-02-07 15:16:44 +0800

    新增数据一条

--- src/com/mall/action/GoddessAction.java
+++ src/com/mall/action/GoddessAction.java
@@ -1,5 +1,6 @@
 package com.mall.action;
 
+import java.util.Date;
 import java.util.List;
 
 import com.mall.dao.GoddessDao;
@@ -12,5 +13,17 @@ public class GoddessAction {
 		for(Goddess goddess:gs){
 			System.out.println(goddess.getUser_name()+","+goddess.getAge());
 		}
+		Goddess g1 = new Goddess();
+		g1.setUser_name("Ð¡¹û¹û");
+		g1.setAge(22);
+		g1.setSex(1);
+		g1.setBirthday(new Date());
+		g1.setEmail("yzcxiaozhi@126.com");
+		g1.setMobile("15898688888");
+		g1.setCreate_user("admin");
+		g1.setUpdate_user("admin");
+		g1.setIsdel(1);
+		
+		g.addGoddess(g1);
 	}
 }
--- src/com/mall/dao/GoddessDao.java
+++ src/com/mall/dao/GoddessDao.java
@@ -1,6 +1,8 @@
 package com.mall.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,8 +13,25 @@ import com.mall.db.DBUtil;
 import com.mall.model.Goddess;
 
 public class GoddessDao {
-	public void addGoddess(){
+	public void addGoddess(Goddess g) throws Exception{
 		Connection  conn = DBUtil.getConnection();
+		String sql = ""+
+				"insert into imooc_goddess"+
+				"(user_name,sex,age,birthday,email,mobile,"+
+				"create_user,create_date,update_user,update_date,isdel)"+
+				"values("+
+				"?,?,?,?,?,?,?,current_date(),?,current_date(),?)";
+		PreparedStatement ptmt = conn.prepareStatement(sql);
+		ptmt.setString(1, g.getUser_name());
+		ptmt.setInt(2, g.getSex());
+		ptmt.setInt(3,g.getAge());
+		ptmt.setDate(4, new Date(g.getBirthday().getTime()));
+		ptmt.setString(5, g.getEmail());
+		ptmt.setString(6, g.getMobile());
+		ptmt.setString(7, g.getCreate_user());
+		ptmt.setString(8, g.getUpdate_user());
+		ptmt.setInt(9, g.getIsdel());
+		ptmt.execute();
 	}
 	
 	public void updateGoddess(){
